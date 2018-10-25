86c2ee9851c24891cc39c3daef3f680038180379
qmayville <qmayville@gmail.com>
2018-10-25 14:46:59 -0500

    added file to process json data and split into training and test json files via random sampling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS328SentimentAnalysis
+
+Data set is too big for GitHub. Should be kept locally in a directory called yelp_dataset.
---
+++ process_data.py
@@ -0,0 +1,52 @@
+import json, random, time
+
+
+def random_sample_yelp_data(n):
+    '''
+    Randomly samples the Yelp review data set into a training data set of size n
+    and a test data set of size n. Writes out the data sets into new files.
+
+    Sample Runtimes for different n values:
+    2500: 533 seconds
+    500: 114 seconds
+    250: 62 seconds
+    25: 8 seconds
+
+    Roughly n/5 seconds
+    '''
+    training_data = []
+    test_data = []
+    random_sample = random.sample(range(1, 5996996), 2*n)
+    random.shuffle(random_sample)
+    training_sample = random_sample[:n]
+    test_sample = random_sample[n:]
+
+    with open('yelp_dataset/yelp_academic_dataset_review.json') as data_file:  # 5,996,996 total lines
+        i = 1
+        size = 0
+        for line in data_file:
+            if i in training_sample:
+                line_data = json.loads(line)
+                training_data.append(line_data)
+                size += 1
+            if i in test_sample:
+                line_data = json.loads(line)
+                test_data.append(line_data)
+                size += 1
+            if size == 2*n:  # If done with sample, stop going through file
+                break
+            i += 1
+
+    with open('test_data/yelp_test_sample_{n}.json'.format(n=n), 'w') as outfile:
+        json.dump(test_data, outfile)
+
+    with open('training_data/yelp_training_sample_{n}.json'.format(n=n), 'w') as outfile:
+        json.dump(training_data, outfile)
+
+
+# sample function on how to get data
+def read_data_from_sample(filename):
+    with open(filename) as data_file:
+        data = json.loads(data_file.read())
+        review = data[0]
+        print(review['text'])
